56e04517e1bb39e53f0e9129f4668b90c9a34067
Ramiro Chacon <ramiro20010202@gmail.com>
2023-01-26 22:12:23 -0600

    Se ordenan los numeros

--- Laboratorio/Practica1/RamiroChacon.c
+++ Laboratorio/Practica1/RamiroChacon.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 
 int main(){
+    printf("Ingrese un numero: ");
     int numeroInferior = 100;
+    printf("Ingrese otro numero: ");
     int numeroSuperior = 5;
 
     if (numeroInferior > numeroSuperior)
@@ -11,4 +13,6 @@ int main(){
         numeroInferior = numeroSuperior;
         numeroSuperior = aux;
     }
+    printf("El numero menor es: %d \n", numeroInferior);
+    printf("El numero mayor es: %d \n", numeroSuperior);
 }
