a45eb408a1e99ae362108d9aa5a968d2f3721f49
Ramiro Chacon <ramiro20010202@gmail.com>
2023-01-26 23:09:54 -0600

    Trabaja practica 1

--- Laboratorio/Practica1/RamiroChacon.c
+++ Laboratorio/Practica1/RamiroChacon.c
@@ -15,4 +15,29 @@ int main(){
     }
     printf("El numero menor es: %d \n", numeroInferior);
     printf("El numero mayor es: %d \n", numeroSuperior);
+
+    int i;
+    for ( i = numeroInferior; i <= numeroSuperior; i++)
+    {
+        int j;
+        int bandera = 0;
+        for ( j = 1; j < numeroSuperior; j++)
+        {
+            if (i % j == 0)
+            {
+                bandera++;
+            }
+
+            if (bandera > 2)        //Los numeros primos solo tienen 2 divisores (1 y ellos mismos), si tienen mas, no son primos.
+            {
+                return 2;
+            }
+        }
+        if (bandera <= 2)
+        {
+            printf("%d",j);
+        }
+        
+    }
+    
 }
