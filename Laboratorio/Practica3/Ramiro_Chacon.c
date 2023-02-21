30503b0852c3989d3a76886a43468951fb2b8318
Ramiro Chacon <ramiro20010202@gmail.com>
2023-02-21 20:44:35 +0000

    Imprime fecha actual

--- Laboratorio/Practica3/Ramiro_Chacon.c
+++ Laboratorio/Practica3/Ramiro_Chacon.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 void creando_Fecha();
 
 int main() {
-
+        creando_Fecha();
         return 0;
 }
 
 void creando_Fecha(){
         time_t tiempo = time(0);
         struct tm *tlocal = localtime(&tiempo);
-        char output[128];
-        strftime(output,128,"%d/%m/%y %H:%M:%S",tlocal);
-        printf("%s\n",output);
-        
+
         struct fecha
         {
-                int dd;
-                int mm;
-                int yy;
+                int day;
+                int month;
+                int year;
 
-                int HH;
-                int MM;
-                int SS;
+                int hour;
+                int minute;
+                int seconds;
         };   
+
+        struct fecha fecha_Actual;
+        struct fecha *puntero_Fecha = malloc(sizeof(fecha_Actual));
+
+        puntero_Fecha->day = tlocal->tm_mday;
+        puntero_Fecha->month = tlocal->tm_mon +1;
+        puntero_Fecha->year = tlocal->tm_year + 1900;
+
+        puntero_Fecha->hour = tlocal->tm_hour - 6;
+        puntero_Fecha->minute = tlocal->tm_min;
+        puntero_Fecha->seconds = tlocal->tm_sec;
+        printf("Hoy es: %d/%d/%d %d:%d:%d\n", puntero_Fecha->day, puntero_Fecha->month, puntero_Fecha->year, puntero_Fecha->hour, puntero_Fecha->minute, puntero_Fecha->seconds); 
 }
