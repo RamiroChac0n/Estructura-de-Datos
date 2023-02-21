c4dba9bd72153a6684206af66be90f9930d7154f
Ramiro Chacon <ramiro20010202@gmail.com>
2023-02-21 17:43:01 +0000

    Crea una función aparte

--- Laboratorio/Practica3/Ramiro_Chacon.c
+++ Laboratorio/Practica3/Ramiro_Chacon.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <time.h>
 
-int main(int argc,char* argv[]) {
+void creando_Fecha();
 
+int main() {
+
+        return 0;
+}
+
+void creando_Fecha(){
         time_t tiempo = time(0);
         struct tm *tlocal = localtime(&tiempo);
         char output[128];
         strftime(output,128,"%d/%m/%y %H:%M:%S",tlocal);
         printf("%s\n",output);
+        
+        struct fecha
+        {
+                int dd;
+                int mm;
+                int yy;
 
-        return 0;
+                int HH;
+                int MM;
+                int SS;
+        };   
 }
