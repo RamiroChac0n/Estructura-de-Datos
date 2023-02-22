5e535c8d3bc070b5e1d7bfcf4d37e47ade2fa0dd
Ramiro Chacon <ramiro20010202@gmail.com>
2023-02-22 01:19:47 +0000

    Soluciona el problema de la hora

--- Laboratorio/Practica3/Ramiro_Chacon.c
+++ Laboratorio/Practica3/Ramiro_Chacon.c
@@ -31,7 +31,13 @@ void creando_Fecha(){
         puntero_Fecha->month = tlocal->tm_mon +1;
         puntero_Fecha->year = tlocal->tm_year + 1900;
 
-        puntero_Fecha->hour = tlocal->tm_hour - 6;
+        if ((tlocal->tm_hour) < 6)
+        {
+                puntero_Fecha->hour = tlocal->tm_hour +18;
+        }else
+        {
+                puntero_Fecha->hour = tlocal->tm_hour - 6;
+        }   
         puntero_Fecha->minute = tlocal->tm_min;
         puntero_Fecha->seconds = tlocal->tm_sec;
         printf("Hoy es: %d/%d/%d %d:%d:%d\n", puntero_Fecha->day, puntero_Fecha->month, puntero_Fecha->year, puntero_Fecha->hour, puntero_Fecha->minute, puntero_Fecha->seconds); 
