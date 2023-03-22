13ed25a6f63570a888870dba65ee1370bb900d15
Ramiro Chacon <ramiro20010202@gmail.com>
2023-03-22 22:30:49 +0000

    Algoritmo creado en clase

--- Clases/Practica2Complemento/ej1.c
+++ Clases/Practica2Complemento/ej1.c
@@ -1,40 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void cargar(int mat[3][5])
 {
-
     int f, c;
+
     for (f = 0; f < 3; f++)
     {
         for (c = 0; c < 5; c++)
         {
-            printf("ingrese un dato:");
-            scanf("%i", mat[f][c]);
+            printf("Ingrese un dato:");
+            scanf("%i", &mat[f][c]);
         }
     }
 }
 
 void imprimir(int mat[3][5])
 {
-
     int f, c;
     for (f = 0; f < 3; f++)
     {
         for (c = 0; c < 5; c++)
         {
-            
-            printf("%i", mat[f][c]);
+            printf("%i ", mat[f][c]);
         }
         printf("\n");
-
     }
-
 }
 
-int main(){
-
-    int mat [3][5];
+int main()
+{
+    int mat[3][5];
     cargar(mat);
     imprimir(mat);
+    getch();
     return 0;
 }
