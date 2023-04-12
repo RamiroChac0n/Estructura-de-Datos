0e25732880ff0c622413fa71895d723b8b104afc
Ramiro Chacon <ramiro20010202@gmail.com>
2023-04-12 00:17:17 +0000

    Termina el ejercicio 6

--- HojasDeTrabajo/01_Punteros/06/06_Ejercicio.c
+++ HojasDeTrabajo/01_Punteros/06/06_Ejercicio.c
@@ -6,11 +6,11 @@ int main(){
     char *a[8] = {"mercurio", "venus", "tierra", "marte", "jupiter", "saturno", "urano", "neptuno"};
     for ( i = 0; i < 8; i++)
     {
-        //printf("Planeta: %s\n", a[i]);
-        //printf("Planeta: %c\n", *(a+3));                // Solo imprime el planeta que está en la posición 3, recordando que se empieza desde 0
-        printf("Planeta: %c\n", **(a));
-        printf("Planeta: %c\n", **(a+2));
-        printf("Planeta: %c\n", **(a+3));
+        printf("Planeta: %s\n", a[i]);                          // Imprime todos los planetas
+        //printf("Planeta: %s\n", *(a+3));                      // Imprime el cuarto planeta    
+        //printf("Planeta: %c\n", **(a));                       // Imprime la primera del primer planeta            
+        //printf("Planeta: %c\n", **(a+2));                     // Imprime la primera del tercer planeta
+        //printf("Planeta: %c\n", **(a+3));                     // Imprime la primera del cuarto planeta
     }
     return 0;
 }
