6b92d4a1a33bee6c997f2d6805569e6a272a925d
Ramiro Chacon <ramiro20010202@gmail.com>
2023-04-14 21:38:05 +0000

    Termina algoritmo de lista

--- Clases/Listas_Enlazadas/01.c
+++ Clases/Listas_Enlazadas/01.c
@@ -26,7 +26,7 @@ struct Pila
     nodo p;
 }pila;
 
-void agregarActualizacion(){
+agregarActualizacion(){
     int edad, dia, mes, anio;
     char nombre[60];
     printf("Ingrese su nombre: \n");
@@ -45,14 +45,43 @@ void agregarActualizacion(){
     printf("Ingrese anio de actualizacion: \n");
     scanf("%d", &anio);     
 
-    strcpy(pila.p[pila.cantidadDatosActuales].empleado.nombre, nombre);
-    pila.p[pila.cantidadDatosActuales].empleado.edad = 25;
-    pila.p[pila.cantidadDatosActuales].f.dia = 27;
-    pila.p[pila.cantidadDatosActuales].f.mes = 3;
-    pila.p[pila.cantidadDatosActuales].f.anio = 2023;     
+    if(pila.cantidadDatosActuales < pila.tamanioPila){
+        strcpy(pila.p[pila.cantidadDatosActuales].empleado.nombre, nombre);
+        pila.p[pila.cantidadDatosActuales].empleado.edad = 25;
+        pila.p[pila.cantidadDatosActuales].f.dia = 27;
+        pila.p[pila.cantidadDatosActuales].f.mes = 3;
+        pila.p[pila.cantidadDatosActuales].f.anio = 2023;     
+    
+        int datosActuales = pila.cantidadDatosActuales;
+        pila.cantidadDatosActuales = datosActuales +1; 
+    }else
+    {
+        printf("Pila esta llena\n");
+    }
+     
+}
 
-    int datosActuales = pila.cantidadDatosActuales;
-    pila.cantidadDatosActuales = datosActuales +1;  
+MostrarUltimaActualizacion(){
+    printf("nombre: %s\nedad: %d\ndia: %d\nmes: %d\nanio: %d\n", pila.p[(pila.cantidadDatosActuales) -1].empleado.nombre,
+                                                                pila.p[(pila.cantidadDatosActuales) -1].empleado.edad,
+                                                                pila.p[(pila.cantidadDatosActuales) -1].f.dia,
+                                                                pila.p[(pila.cantidadDatosActuales) -1].f.mes,
+                                                                pila.p[(pila.cantidadDatosActuales) -1].f.anio);
+}
+
+EliminarActualizacion(){
+    strcpy(pila.p[(pila.cantidadDatosActuales) -1].empleado.nombre, " ");
+            pila.p[(pila.cantidadDatosActuales) -1].f.dia = 0;
+            pila.p[(pila.cantidadDatosActuales) -1].empleado.edad = 0;
+            pila.p[(pila.cantidadDatosActuales) -1].f.mes = 0;
+            pila.p[(pila.cantidadDatosActuales) -1].f.anio = 0;
+            int cantDatos = pila.cantidadDatosActuales;
+
+            if (pila.cantidadDatosActuales > 0)
+            {
+                pila.cantidadDatosActuales = cantDatos -1; 
+            }
+            
 }
 
 int main(){
@@ -68,13 +97,35 @@ int main(){
     pila.p[1].f.mes = 3;
     pila.p[1].f.anio = 2023;
 
-    strcpy(pila.p[2].empleado.nombre, "Pedro");
+    strcpy(pila.p[2].empleado.nombre, "Ricardo");
     pila.p[2].empleado.edad = 25;
-    pila.p[2].f.dia = 27;
+    pila.p[2].f.dia = 29;
     pila.p[2].f.mes = 3;
     pila.p[2].f.anio = 2023;
 
     pila.tamanioPila = 10;
     pila.cantidadDatosActuales = 3;
-    return 0;
+
+    int opc;
+    char continuar;
+    do{
+        //system("cls");
+        printf("Ingrese la operación que desea realizar\n");
+        printf("(1) Agregar nueva actualización\n");
+        printf("(2) Eliminar actualización\n");
+        printf("(3) Mostrar ultima actualización\n");
+        scanf("%i", &opc);
+
+        if(opc == 1){
+            agregarActualizacion();
+        }else if(opc == 2){
+            EliminarActualizacion();
+        }else if(opc == 3){
+            MostrarUltimaActualizacion();
+        }
+
+        printf("Desea continuar Si (s) No (n)\n");
+        scanf(" %c", &continuar);
+    }while(continuar == 's' || continuar == 'S');
+    //system("pause");
 }
