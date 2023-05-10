264138a4003cfb22dbac1c471b19afe0e6b55b7c
Ramiro Chacon <ramiro20010202@gmail.com>
2023-05-10 23:07:14 +0000

    Agrega explicacion del algoritmo

--- Laboratorio/Practica7/matriz_adyacencia.c
+++ Laboratorio/Practica7/matriz_adyacencia.c
@@ -1,79 +1,79 @@
-#include <stdio.h>
+#include <stdio.h>                                              //Importa las librerias necesarias.
 
-int main(){
+int main(){                                                     //Funcion principal.
 
-    int tam, contador, contador2, contador3;
-    char a = 97;
+    int tam, contador, contador2, contador3;                    //Declaracion de variables.
+    char a = 97;                                                //Declaracion de variable para imprimir el abecedario.
 
-    printf("Dame el numero de nodos: \n");
-    scanf("%d", &tam);
-    int arreglo[tam][tam];
-    printf(" ");
+    printf("Dame el numero de nodos: \n");                      //Pide el numero de nodos.
+    scanf("%d", &tam);                                          //Guarda el numero de nodos.
+    int arreglo[tam][tam];                                      //Declaracion de arreglo bidimensional.
+    printf(" ");                                                //Imprime un espacio en blanco.
 
-    for (contador3 = 0; contador3 < tam; contador3++)
+    for (contador3 = 0; contador3 < tam; contador3++)           //Ciclo for para imprimir el abecedario.
     {
-        printf("%c", a++);
+        printf("%c", a++);                                      //Imprime el abecedario.
     }
 
-    printf("\n");
-    a = 97;
+    printf("\n");                                               //Imprime un salto de linea.
+    a = 97;                                                     //Reinicia la variable a = 97.
 
-    for (contador2 = 0; contador2 < tam; contador2++)
+    for (contador2 = 0; contador2 < tam; contador2++)           //Ciclo for para imprimir el abecedario y el arreglo.
     {
-        printf("%c", a++);
-        for (contador = 0; contador < tam; contador++)
+        printf("%c", a++);                                      //Imprime el abecedario.
+        for (contador = 0; contador < tam; contador++)          //Ciclo for para imprimir el arreglo.
         {
-            arreglo[contador][contador2] = 0;
-            printf("%d", arreglo[contador][contador2]);
+            arreglo[contador][contador2] = 0;                   //Inicializa el arreglo en 0.
+            printf("%d", arreglo[contador][contador2]);         //Imprime el arreglo.
         }
-        printf("\n");
+        printf("\n");                                           //Imprime un salto de linea.
     }
 
-    char x, y;
-    int nx, ny;
-    char resp = 's';
+    char x, y;                                                  //Declaracion de variables.
+    int nx, ny;                                                 //Declaracion de variables.
+    char resp = 's';                                            //Declaracion de variables.
 
-    do
+    do                                                          //Ciclo do while para ingresar las intersecciones.
     {
-        fflush(stdin);
-        printf("Ingresa la letra (x):\n");
-        scanf("%c", &x);
-        printf("Ingrese la (y):\n");
-        fflush(stdin);
-        scanf("%c", &y);
+        fflush(stdin);                                          //Limpia el buffer.
+        printf("Ingresa la letra (x):\n");                      //Pide la letra x.
+        scanf("%c", &x);                                        //Guarda la letra x.
+        printf("Ingrese la (y):\n");                            //Pide la letra y.
+        fflush(stdin);                                          //Limpia el buffer.       
+        scanf("%c", &y);                                        //Guarda la letra y.
 
-        nx = x -97;
-        ny = y -97;
-        arreglo[nx][ny] = 1;
+        nx = x -97;                                             //Convierte la letra x a numero.
+        ny = y -97;                                             //Convierte la letra y a numero.
+        arreglo[nx][ny] = 1;                                    //Guarda el 1 en la posicion nx, ny.
 
-        a = 97;
-        printf(" ");
+        a = 97;                                                 //Reinicia la variable a = 97.
+        printf(" ");                                            //Imprime un espacio en blanco.
 
-        for (contador3 = 0; contador3 < tam; contador3++)
+        for (contador3 = 0; contador3 < tam; contador3++)       //Ciclo for para imprimir el abecedario.
         {
-            printf("%c", a++);
+            printf("%c", a++);                                  //Imprime el abecedario.
         }
-        printf("\n");
-        a = 97;
+        printf("\n");                                           //Imprime un salto de linea.
+        a = 97;                                                 //Reinicia la variable a = 97.
         
-        for (contador2 = 0; contador2 < tam; contador2++)
+        for (contador2 = 0; contador2 < tam; contador2++)       //Ciclo for para imprimir el abecedario y el arreglo.
         {
-            printf("%c", a++);
+            printf("%c", a++);                                  //Imprime el abecedario.
             //a = 97;
 
-            for (contador = 0; contador < tam; contador++)
+            for (contador = 0; contador < tam; contador++)      //Ciclo for para imprimir el arreglo.
             {
                 //arreglo[contador][contador2] = 0;
-                printf("%d", arreglo[contador][contador2]);
+                printf("%d", arreglo[contador][contador2]);     //Imprime el arreglo.
             }
-            printf("\n");
+            printf("\n");                                       //Imprime un salto de linea.
         }
 
-        printf("Deseas ingresar otra interseccion? s/n:\n");
-        fflush(stdin);
-        scanf("%c", &resp);
+        printf("Deseas ingresar otra interseccion? s/n:\n");    //Pregunta si desea ingresar otra interseccion.
+        fflush(stdin);                                          //Limpia el buffer.
+        scanf("%c", &resp);                                     //Guarda la respuesta.
         
 
-    } while (resp != 'n' && resp != 'N');
-    return 0;
+    } while (resp != 'n' && resp != 'N');                       //Condicion para que se repita el ciclo.
+    return 0;                                                   //Retorna 0.
 }
